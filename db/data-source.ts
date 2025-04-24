34e8c859622bb3d2d3337fb57c50aa46c376c5b3
Shkettoe <denis.tosic@proton.me>
2025-04-24 19:31:29 +0200

    dockerization

--- db/data-source.ts
+++ db/data-source.ts
@@ -8,11 +8,13 @@ const configService = new ConfigService();
 
 export default new DataSource({
   type: 'postgres',
-  host: configService.get('DB_HOST'),
+  host: '35.246.201.187',
+  /* host: configService.get('DB_HOST'),*/
   port: configService.get<number>('DB_PORT'),
   username: configService.get('DB_USER'),
   password: configService.get('DB_PASSWORD'),
-  database: configService.get('DB_DATABASE'),
+  /* database: configService.get('DB_DATABASE'), */
+  database: 'travel_planner',
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: false,
   migrations: ['dist/db/migrations/*{.ts,.js}'],
